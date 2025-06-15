5f16827bf8d4ebc48dfae1f3ab211129e9b06eb4
Colleen <colleen.c.cranna.28@dartmouth.edu]>
2025-06-15 13:46:20 -0400

    determined coordinates for counties of focus, updated map css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import stateImage from './images/washington-state-image.jpg';
 import './App.css';
+import React, {useState} from 'react';
 
 
 function App() {
+  const [region, setRegion] = useState(null); //designates which county user wants info about
+  const [info, setInfo] = useState(null); //filters information for popup window
+  const washMap = {
+    areas: [
+      { name: "Olympia", shape: "rect", coords: [140,194,154,207] }, //coordinates from IMG mapper tool
+      { name: "Spokane", shape: "circle", coords: [458,142,11] },
+      {name: "Seattle", shape: "circle", coords: [176,147,14]},
+      { name: "Yakima", shape: "circle", coords: [279,233,16]},
+    ],
+  }
+
+
   return (
-    <div>
-      <img src={stateImage} alt="Washington State" style={{width: '100%', height: 'auto'}}/>
+    <div className="washingtonMap">
+      <img 
+        src={stateImage}
+        alt="Washington State Map" 
+      />
     </div>
   );
 }
